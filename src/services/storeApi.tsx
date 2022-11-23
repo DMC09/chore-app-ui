d44c0f9c65814063f8708e2879d3cabf160210fb
Dharmin <dharmin@seatzapp.com>
2022-11-22 20:13:04 -0600

    changed type for query

--- src/services/storeApi.tsx
+++ src/services/storeApi.tsx
@@ -1,12 +1,12 @@
 import { EndpointBuilder } from "@reduxjs/toolkit/dist/query/endpointDefinitions"
 import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
-import { StoreInterface } from "../models/store"
+import { allStoresResponseModel, StoreInterface } from "../models/store"
 
 export const storesApi = createApi({
     reducerPath:"storesApi",
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
     endpoints:(builder) =>   ({
-        getStores:builder.query<StoreInterface[],void>({
+        getStores:builder.query<allStoresResponseModel,void>({
             query:()=> '/stores'
         })
     })
