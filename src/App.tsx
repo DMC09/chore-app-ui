f975432f7fffde752e508733c8a663714082ce6b
Dharmin <dharmin@seatzapp.com>
2022-11-22 20:13:15 -0600

    can see data from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import { Stores } from './features/store/Store';
 import { useAppSelector, useAppDispatch } from './app/hooks';
-import { selectStores } from './features/store/storeSlice';
 import {useGetStoresQuery} from './services/storeApi'
 
 function App() {
 const {data,error,isLoading, isFetching, isSuccess} = useGetStoresQuery()
+
+const storeToDisplay = data?.data
+
+
 
   // const stores = useAppSelector(selectStores);
   // const dispatch = useAppDispatch();
@@ -17,18 +19,18 @@ const {data,error,isLoading, isFetching, isSuccess} = useGetStoresQuery()
     // This is the overall container for the, need to change to something else, ??
     // change this to a store. 
           console.log('component mounted!')  
-          console.log(stores)
-        },[])
+          console.log(error,'error')  
+          console.log(JSON.stringify(storeToDisplay, null, 2),'stringifcy')
+        })
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <Stores  />
-      </header>
-    </div>
+    <>
+      {isLoading && <h2>  is Loading</h2>}
+      {isFetching && <h2>  is Fetching</h2>}
+      {error && <h2>  something went wrong</h2>}
+      {isSuccess && <p> We got a hit!</p>}
+    </>
   );
 }
 
